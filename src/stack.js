f61b7547368db4fa1a39fcd115dd00605ce18b75
Denis Bernovich <denisbernovich@gmail.com>
2022-10-17 21:58:11 +0300

    feat: completed stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  newArr = [];
+  push(element) {
+    this.newArr.push(element)
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  pop(element) {
+    return this.newArr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.newArr[this.newArr.length - 1];
   }
 }
 
